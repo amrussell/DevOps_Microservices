eab08fd49c7c981464fffbe3c568935845e2b8b3
Alex Russell <alex.russell.tx@gmail.com>
2021-04-01 18:31:26 -0500

    can we do it in one commit?

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=amrussell/udacityproject4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment --image=${dockerpath} flask-app
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+NEW_POD=$(kubectl get pods | awk '{print $1}' | awk 'NR==2')
+sleep 30s
+kubectl get pods
+kubectl port-forward $NEW_POD 8000:80 
 
